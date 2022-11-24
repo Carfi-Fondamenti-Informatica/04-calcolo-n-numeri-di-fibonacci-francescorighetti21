23ae0219465c01eeff0435f9a581bef784de3cd9
francescorighetti21 <117826738+francescorighetti21@users.noreply.github.com>
2022-11-24 18:58:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
+   int n=0;
+   int result=0;
+   int precedente=1;
+   int successivo=1;
+   cin >> n;
+   if(n<2){
+      cout << "errore" << endl;
+      return 0;
+     }
+   cout << precedente << endl << successivo << endl;
+   for(int i=o; i<n;++1){
+      result=precedente+successivo;
+      precedente0successivo;
+      successivo=result;
+      cout << result << endl;
+   }
+
+   
+   
+   
+   
    return 0;
 }
